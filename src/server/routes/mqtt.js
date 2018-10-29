ceb84cc307a7ef6677b20e0e8badea378b9b3fc3
stevevista <kencube@hotmail.com>
2018-10-29 18:29:48 +0800

    fix get params from ctx.req.body

--- src/server/routes/mqtt.js
+++ src/server/routes/mqtt.js
@@ -4,17 +4,17 @@ const Router = require('koa-router')
 const router = Router()
 
 router.post('/auth', async ctx => {
-
+  const {username, password, topic, acc} = ctx.request.body
   const dbuser = await ctx.db.users.findOne({
     where: {
-      username: ctx.request.username
+      username
     }
   })
 
-  const authed = dbuser && await dbuser.checkPassword(ctx.request.password)
+  const authed = dbuser && await dbuser.checkPassword(password)
   ctx.assert(authed, 401, 'wrong user or password')
 
-  console.log(ctx.request.username, ctx.request.password, ctx.request.topic, ctx.request.acc)
+  console.log(topic, acc)
   ctx.body = {}
 })
 
@@ -22,16 +22,17 @@ router.post('/superuser', async ctx => {
 
   const dbuser = await ctx.db.users.findOne({
     where: {
-      username: ctx.request.username
+      username: ctx.request.body.username,
+      level: 0
     }
   })
-  ctx.assert(dbuser && dbuser.level === 0 && !dbuser.disabled, 403)
+  ctx.assert(dbuser && !dbuser.disabled, 403)
   ctx.body = {}
 })
 
 router.post('/acl', async ctx => {
-
-  console.log(ctx.request.username, ctx.request.clientid, ctx.request.topic, ctx.request.acc)
+  const {username, clientid, topic, acc} = ctx.request.body
+  console.log(username, clientid, topic, acc)
   ctx.body = {}
 })
 
